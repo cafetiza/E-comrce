0df9f51eb5866dd7881025547a6284f1ac76a901
John <cafetiza@gmail.com>
2022-02-09 22:55:04 -0600

    E-comrece 278

--- proyecto-poo/views/layout/header.php
+++ proyecto-poo/views/layout/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css" />
+    <link rel="stylesheet" href="<?= base_url ?>assets/css/styles.css" />
     <title>Proyecto poo</title>
 </head>
 
@@ -14,34 +14,27 @@
         <!-- CABECERA -->
         <header id="header">
             <div id="logo">
-                <img src="<?=base_url?>assets/img/camiseta.png" alt="Camiseta Logo" />
-				<a href="<?=base_url?>">
+                <img src="<?= base_url ?>assets/img/camiseta.png" alt="Camiseta Logo" />
+                <a href="<?= base_url ?>">
                     Tienda de camisetas
                 </a>
             </div>
         </header>
 
         <!-- MENU -->
+        <?php $categorias = Utils::showCategorias(); ?>
         <nav id="menu">
             <ul>
                 <li>
                     <a href="#">Inicio</a>
                 </li>
-                <li>
-                    <a href="#">Categoria 1</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 2</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 3</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 4</a>
-                </li>
-                <li>
-                    <a href="#">Categoria 5</a>
-                </li>
+
+                <?php while ($cat = $categorias->fetch_object()) : ?>
+                    <li>
+                        <a href="#"><?=$cat->nombre?></a>
+                    </li>
+                <?php endwhile; ?>
+
             </ul>
         </nav>
 
